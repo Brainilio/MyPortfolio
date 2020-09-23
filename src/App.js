30fc70b8a4226bd88789af37aa9237f72207d00f
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-09-23 10:02:18 +0200

    clean up project to start fresh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React from "react"
-import Nav from "./components/nav"
-import Button from "./components/button"
-import Withclass from "./hoc/container"
-import Particles from "react-particles-js"
-import Title from "./components/title"
 
 function App() {
-	return (
-		<>
-			<Nav />
-
-			<div className='landing-section'>
-				<Title />
-			</div>
-
-			{/* <Particles /> */}
-			{/* <Button /> */}
-		</>
-	)
+	return <></>
 }
 
 export default App
--- src/components/button.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const Button = (props) => {
-	return <button>Click me</button>
-}
-
-export default Button
