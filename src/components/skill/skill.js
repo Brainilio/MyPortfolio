e1a23517f12fa5b486394b27619d9ec9a49e7ea1
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-09-23 18:14:53 +0200

    skill section halfway done

--- src/components/skill/skill.js
+++ src/components/skill/skill.js
@@ -1,7 +1,84 @@
 import React from "react"
+import "./skill.scss"
 
 const skill = () => {
-	return <div></div>
+	return (
+		<section className="my-skills">
+			<div className="skills-title">
+				<div className="skills-title-block"></div>
+				<span className="skills-title-title" aria-hidden>
+					MY SKILLS 🛠
+				</span>
+			</div>
+
+			<div className="skills-column">
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+				<div>
+					<div>@</div>
+					<span> REACTJS </span>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default skill
