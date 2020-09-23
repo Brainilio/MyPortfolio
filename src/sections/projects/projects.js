290c3d9e69beb61a5f0b959a8d8ca23fac7aeab5
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-09-23 12:45:13 +0200

    projects

--- src/sections/projects/projects.js
+++ src/sections/projects/projects.js
@@ -1,9 +1,13 @@
 import React from "react"
+import "./projects.scss"
 
 const projects = () => {
 	return (
 		<section className="projects">
-			<div>Hi</div>
+			<div className="projects-title">
+				<div className="projects-title-block"></div>
+				<span className="projects-title-title">MY WORK</span>
+			</div>
 		</section>
 	)
 }
