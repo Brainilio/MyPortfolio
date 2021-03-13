282cdbae74ebfa9ad4e7610a7c12b3b1c10b6299
B.A. Imambaks Rodrigues <0924777@hr.nl>
2021-03-13 17:26:13 +0100

    adjustments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { useState, useEffect } from "react"
-import Particles from "react-particles-js"
 import Rocketship from "./components/rocketship/rocketship"
 import { Switch, Route, useLocation } from "react-router-dom"
 import ReactGA from "react-ga"
 import ProjectPage from "./pages/projectpage/ProjectPage"
 import ProjectDetail from "./sections/projects/projectDetail"
 import Main from "./sections/main"
-// import { ReactComponent as TextSpinning } from "./resources/spinnertext.svg"
-import { Canvas, useFrame } from "react-three-fiber"
+import { Canvas, extend, useFrame, useThree } from "react-three-fiber"
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer"
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass"
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { Suspense } from "react"
-import LoadingScreen from "./components/LoadingScreen/LoadingScreen"
+import { OrbitControls } from "@react-three/drei"
+extend({ EffectComposer, RenderPass, UnrealBloomPass })
 
 const ThreeModels = (props) => {
 	const [moon, setMoon] = useState(null)
+	const [space, setSpace] = useState(null)
 	const [earth, setEarth] = useState(null)
+	const [astro, setAstro] = useState(null)
+	const { camera } = useThree()
+	const [shouldTurn, setShouldTurn] = useState(false)
 
 	useEffect(() => {
-		new GLTFLoader().load("moon/scene.gltf", setMoon)
-		new GLTFLoader().load("earth/scene.gltf", setEarth)
+		new GLTFLoader().load("rock/scene.gltf", setMoon)
+		new GLTFLoader().load("need_some_space/scene.gltf", setSpace)
+		new GLTFLoader().load("planet/scene.gltf", setEarth)
+		new GLTFLoader().load("astronaut_helmet/scene.gltf", setAstro)
 	}, [])
 
 	// Rotate mesh every frame, this is outside of React without overhead
@@ -26,22 +34,62 @@ const ThreeModels = (props) => {
 		if (earth) {
 			earth.scene.rotation.y += 0.002
 		}
+		if (space) {
+			space.scene.rotation.y += 0.00003
+			space.scene.position.z += 0.001
+		}
+		if (astro && !shouldTurn && astro.scene.rotation.y > 2.5) {
+			astro.scene.rotation.y -= 0.001
+		}
+
+		if (astro && astro.scene.rotation.y <= 2.5) {
+			setShouldTurn(true)
+		}
+
+		if (astro && shouldTurn && astro.scene.rotation.y < 3.6) {
+			astro.scene.rotation.y += 0.001
+		}
+
+		if (astro && astro.scene.rotation.y >= 3.6) {
+			setShouldTurn(false)
+		}
+		if (moon) {
+			// moon.scene.position.z += 5
+		}
 	})
 
-	if (earth && moon) {
-		props.ToggleRest()
-	}
-
-	return earth && moon ? (
+	return earth && space && moon && astro ? (
 		<>
 			<primitive
-				rotation={[0, 0, 0]}
-				position={[0, -800 + props.posY * 0.05, 0]}
-				object={moon.scene}
+				receiveShadow
+				position={[-100, -180, 100 + props.posY * 0.002]}
+				rotation={[0.5, 0, 0]}
+				object={space.scene}
 			/>
 			<primitive
-				position={[0, 0, 300 - props.scrollPos * 0.04]}
+				receiveShadow
+				position={[-2, -2.3, 30.5 - props.posY * 0.0005]}
+				rotation={[0, 3.6, 6]}
+				scale={[0.05, 0.05, 0.05]}
+				object={astro.scene}
+			/>
+
+			<primitive
+				rotation={[0, 0, 0]}
+				position={[150, -1200 + props.posY * 0.1, 20]}
+				scale={[0.2, 0.2, 0.2]}
+				object={moon.scene}
+			/>
+
+			<primitive
+				position={[0, 0, 28 - props.posY * 0.0005]}
+				rotation={[
+					0,
+					(earth.scene.rotation.y += props.scrollPos * 0.000002),
+					0,
+				]}
 				object={earth.scene}
+				castShadow
 			/>
 		</>
 	) : null
@@ -50,7 +98,6 @@ const ThreeModels = (props) => {
 function App() {
 	let location = useLocation()
 	const [scrollPos, setScrollPos] = useState(0)
-	const [showRest, setShowRest] = useState(false)
 	ReactGA.initialize(`${process.env.REACT_APP_TRACKING_URL}`)
 	ReactGA.pageview("/")
 
@@ -62,54 +109,27 @@ function App() {
 	}, [])
 
 	const scrollhandler = () => setScrollPos(window.pageYOffset)
-	const ToggleRest = () => setShowRest(true)
 
 	return (
 		<div>
 			<Rocketship />
 			<div className="three-canvas">
-				<Suspense fallback="">
-					<Canvas pixelRatio="1" camera={{ position: [0, 30, 500] }}>
-						<ambientLight position={[0, 30, 500]} />
-						<ambientLight intensity={0.2} />
-						<directionalLight intensity={1} />
-						<ThreeModels
-							ToggleRest={ToggleRest}
-							scrollPos={scrollPos}
-							posY={scrollPos}
+				<Canvas camera={{ position: [0, 0, 30] }}>
+					<Suspense fallback="">
+						<OrbitControls />
+						<pointLight intensity={2} />
+						<spotLight
+							intensity={0.5}
+							position={[70, 70, 70]}
+							penumbra={5}
+							color="lightblue"
 						/>
-					</Canvas>
-				</Suspense>
+						<ambientLight intensity={0.3} />
+						<spotLight intensity={1} position={[0, 0, 600]} />
+						<ThreeModels scrollPos={scrollPos} posY={scrollPos} />
+					</Suspense>
+				</Canvas>
 			</div>
-			<Particles
-				className="particles-background"
-				width="100vw"
-				params={{
-					interactivity: {
-						detectsOn: "canvas",
-						events: {
-							resize: true,
-						},
-					},
-					particles: {
-						number: {
-							value: 150,
-							density: {
-								enable: true,
-								value_area: 2000,
-							},
-						},
-						move: {
-							speed: 0,
-							random: false,
-						},
-						links: {
-							enable: false,
-						},
-					},
-				}}
-			/>
-
 			<Switch location={location}>
 				<Route path="/" exact component={Main} />
 				<Route path="/projects" component={ProjectPage} />
@@ -119,4 +139,4 @@ function App() {
 	)
 }
 
-export default React.memo(App)
+export default App
